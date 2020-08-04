ca6a777263c0bf5a3b1c64a7870dbcd4c833f898
Luciano Liboni <luciano_liboni@hotmail.com>
2020-08-04 12:33:00 -0300

    feat: create classes

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.json({ message: "Hehehehe"});
-})
+app.use(routes);
 
 app.listen(3333);
